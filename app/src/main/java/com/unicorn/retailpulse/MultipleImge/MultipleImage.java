e32aeb345a6c8d8884f5950b8300ab364aa4aa14
DELL <harshrajnit@gmail.com>
2020-05-30 22:35:02 +0530

    ui updated1

--- app/src/main/java/com/unicorn/retailpulse/MultipleImge/MultipleImage.java
+++ app/src/main/java/com/unicorn/retailpulse/MultipleImge/MultipleImage.java
@@ -14,6 +14,7 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -31,6 +32,7 @@ public class MultipleImage extends AppCompatActivity {
     private Button btn_multichoose;
     private  ImageFragmentPagerAdapter imageFragmentPagerAdapter;
     private ViewPager viewPager;
+    public static final String TAG="MultiImage";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -94,6 +96,7 @@ public class MultipleImage extends AppCompatActivity {
                 catch (FileNotFoundException e)
                 {
                     e.printStackTrace();
+                    //Log.e(TAG, "onActivityResult: "+e.toString());
                 }
             }
             imageFragmentPagerAdapter = new ImageFragmentPagerAdapter(MultipleImage.this,bitmaps);
